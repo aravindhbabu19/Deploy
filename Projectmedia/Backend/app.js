087a69f9563fb26db0f412120851e53a2695877f
aravindhbabu19 <162553766+aravindhbabu19@users.noreply.github.com>
2025-04-08 13:02:21 +0530

    Update app.js

--- Projectmedia/Backend/app.js
+++ Projectmedia/Backend/app.js
@@ -28,6 +28,11 @@ app.use(cors(
 ))
 
 app.options('*', cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // Update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use('/api/v1/',Baby)
